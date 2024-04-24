85a71dd635101db15f70d40f24ade106eccb52f1
kashyaba <iamkashyab@gmail.com>
2024-04-24 03:39:18 -0700

    onClicks and onChange events for the searchbar

--- src/components/ui/searchbutton.tsx
+++ src/components/ui/searchbutton.tsx
@@ -1,10 +1,19 @@
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
+import { useState } from "react"
 
 export function SearchButton() {
+    const [searchQuery, setSearchQuery] = useState<string>("")
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setSearchQuery(event.target.value)
+    }
+    const handleSeekClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("clicked")
+    }
     return (
         <div className="grid w-full gap-2">
-            <Textarea placeholder="Search for apps" />
+            <Textarea placeholder="Search for apps" onChange={handleChange} value={searchQuery}  />
+            <Button onClick={handleSeekClick}>Seek</Button>
         </div>
     )
 }

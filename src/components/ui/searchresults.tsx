5431a4a115c07a357a815d0e972ffb39ea1450f5
kashyaba <iamkashyab@gmail.com>
2024-05-02 23:47:46 -0700

    fetch clicked result

--- src/components/ui/searchresults.tsx
+++ src/components/ui/searchresults.tsx
@@ -4,11 +4,14 @@ import { Button } from "@/components/ui/button";
 
 export function SearchResults() {
     const { searchResults } = useContext(SearchResultsCtx)
+    const resultOnClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, searchResult: string) => {
+        console.log(searchResult)
+    }
     return (
-        searchResults.map((searchResultEntry: string[]) => {
+        searchResults.map((searchResultEntry: string[], index: number) => {
             const [searchResult, simScore] = searchResultEntry
             return (
-                <Button>
+                <Button key={index} onClick={(event) => resultOnClick(event, searchResult)}>
                     {`${searchResult} - ${simScore}`}
                 </Button>
             )

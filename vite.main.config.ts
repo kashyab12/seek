b05f89af9aec16aa5b8a80f1b4303f6e5626e409
kashyaba <iamkashyab@gmail.com>
2024-05-15 23:16:19 -0700

    allow debugging main proc

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -19,6 +19,7 @@ export default defineConfig((env) => {
       rollupOptions: {
         external,
       },
+      sourcemap: true
     },
     plugins: [pluginHotRestart('restart'), react()],
     define,

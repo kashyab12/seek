555f290e8f17e23076661cb0f07faa196b88e66b
kashyaba <iamkashyab@gmail.com>
2024-05-24 02:48:18 -0700

    refactor: desktop file parser and using node fs instead of bash where possible

--- src/scripts/installed-apps.ts
+++ src/scripts/installed-apps.ts
@@ -1,6 +1,7 @@
 import { exec } from "child_process"
 import { promisify } from "util";
 import { promises as fs } from 'fs';
+import { App } from "electron";
 
 const execAsync = promisify(exec)
 
@@ -8,91 +9,138 @@ export interface InstalledAppsInfo {
     desktopPath: string;
     iconPath: string;
     b64Icon: string;
-    // execCmd: string
+    execCmd: string
+}
+
+const enum DesktopFilePaths {
+    AppsUsrShare = "/usr/share/applications",
+    IconsUsrShare = "/usr/share/icons",
+    AppsLocalShare = "/.local/share/applications",
+    IconsLocalShare = "/.local/share/icons",
+    AppsSnap = "/var/lib/snapd",
+    IconsSnap = "/var/lib/snapd/desktop/icons",
+    AppsFlatpak = "/var/lib/flatpak",
+    IconsFlatpak = "/var/lib/flatpak/desktop/icons"
+}
+
+const enum DesktopFileProps {
+    Name = "Name",
+    Type = "Type",
+    Icon = "Icon",
+    Exec = "Exec",
+    // todo: use this as keywords in the search
+    Categories = "Categories",
+    // terminal app?
+    Terminal = "Terminal",
+    // keywords?
+    Comment = "Comment",
+    // todo: keyword for search
+    GenericName = "GenericName"
+}
+
+export interface DesktopFile {
+    name: string;
+    icon: string;
+    exec: string;
+    type: string;
 }
 
 export type AppInfo = Record<string, InstalledAppsInfo>
 
 // todo: use worker threads to make concurrent.
-async function findAppIcon(desktopPath: string) {
-    const iconsDir = "/usr/share/icons"
-    const iconPathCmd = `grep Icon ${desktopPath}`
+async function findAppIcon(desktopPath: string, desktopFile: DesktopFile, electronAppInfo: App) {
     try {
-        const { stdout: getIconNameStdout, stderr: getIconNameStderr } = await execAsync(iconPathCmd)
-        if (getIconNameStderr) {
-            console.log(getIconNameStderr)
-        }
-        try {
-            const iconName = getIconNameStdout.split("=").at(-1).split("\n").at(0)
-            const getAppIconPathCmd = `find ${iconsDir} -name '${iconName}.*' -type f`
-            const { stdout: getAppIconPathStdout, stderr: getAppIconPathStderr } = await execAsync(getAppIconPathCmd)
-            if (getAppIconPathStderr) {
-                console.log(getAppIconPathStderr)
-            }
-            return getAppIconPathStdout
-        } catch (getAppIconPathError) {
-            console.log(getAppIconPathError)
-            return ""
-        }
-    } catch (getAppIconError) {
-        console.log(getAppIconError)
+        if (desktopFile.icon.includes("/")) return desktopFile.icon
+        let getAppIconPathCmd: string;
+        if (desktopPath.includes(DesktopFilePaths.AppsUsrShare)) {
+            getAppIconPathCmd = `find ${DesktopFilePaths.IconsUsrShare} -name '${desktopFile.icon}.*' -type f`
+        } else if (desktopPath.includes(DesktopFilePaths.AppsLocalShare)) {
+            getAppIconPathCmd = `find ${electronAppInfo.getPath("home")}${DesktopFilePaths.IconsLocalShare} -name '${desktopFile.icon}.*' -type f`
+        } else if (desktopPath.includes(DesktopFilePaths.AppsSnap)) {
+            getAppIconPathCmd = `find ${DesktopFilePaths.IconsSnap} -name '${desktopFile.icon}.*' -type f`
+        } else if (desktopPath.includes(DesktopFilePaths.AppsFlatpak)) {
+            getAppIconPathCmd = `find ${DesktopFilePaths.IconsFlatpak} -name '${desktopFile.icon}.*' -type f`
+        } else throw new EvalError('could not find any icon')
+        const { stdout: getAppIconPathStdout, stderr: getAppIconPathStderr } = await execAsync(getAppIconPathCmd)
+        if (getAppIconPathStderr) console.log(getAppIconPathStderr)
+        return getAppIconPathStdout.replace("\n", "").trim()
+    } catch (getAppIconPathError) {
+        console.log(getAppIconPathError)
         return ""
     }
 }
 
-// async function getAppExecCmd(desktopPath: string) {
-//     const grepExecCmd = `grep Exec ${desktopPath}`
-//     const getExecCmd = fork('/bin/sh', ['-c', grepExecCmd])
-//     let execCmds = ""
-//     for await (const chunk of getExecCmd.stdout) {
-//         execCmds += decoder.decode(chunk)
-//     }
-//     if (!execCmds) {
-//         return ""
-//     }
-//     const filteredExecs = []
-//     for (const cmd of execCmds.split("\n")) {
+async function imgToB64(imagePath: string) {
+    if (!imagePath) return ""
+    const imgData = await fs.readFile(imagePath)
+    const b64Data = Buffer.from(imgData).toString('base64')
+    const imgExt = imagePath.split(".").at(-1)
+    const mimeType = imgExt === "svg" ? "svg+xml" : imgExt
+    return `data:image/${mimeType};base64,${b64Data}`
+}
 
-//     }
-
-// }
-
-async function getInstalledApps() {
+async function getInstalledApps(electronAppInfo: App) {
     try {
-        const findDesktopFilesCmd = 'dpkg --search "*.desktop" | grep /usr/share/applications | sort --unique'
+        // sifting through diff locations where .desktop files would exist
+        const validPaths = [DesktopFilePaths.AppsUsrShare, `${electronAppInfo.getPath("home")}${DesktopFilePaths.AppsLocalShare}`, DesktopFilePaths.AppsSnap, DesktopFilePaths.AppsFlatpak]
+        const findDesktopFilesCmd = `locate "*.desktop"`
         const { stdout: findDesktopFilesStdout, stderr: findDesktopFilesStderr } = await execAsync(findDesktopFilesCmd)
-        if (findDesktopFilesStderr) {
-            console.log(findDesktopFilesStderr)
-        }
-        return findDesktopFilesStdout
+        if (findDesktopFilesStderr) console.log(findDesktopFilesStderr)
+        return findDesktopFilesStdout.split("\n").filter(desktopPath => {
+            validPaths.some(validPath => desktopPath.includes(validPath))
+        })
     } catch (findDesktopFilesError) {
         console.log(findDesktopFilesError)
-        return ""
+        return []
     }
 }
 
-export async function generateInstalledAppsInfo() {
-    const installedApps = await getInstalledApps()
+async function desktopFileParser(desktopFilePath: string) {
+    const desktopFile: DesktopFile = {
+        name: "",
+        icon: "",
+        exec: "",
+        type: ""
+    }
+    try {
+        const unstructDesktop: string = await fs.readFile(desktopFilePath, { encoding: 'utf-8' })
+        const props: string[] = unstructDesktop.split('\n').filter(line => line.trim() !== '' && !line.startsWith("#"));
+        for (const desktopProp of props) {
+            const [desktopKey, desktopValue] = desktopProp.split("=", 1)
+            switch (desktopKey.trim()) {
+                case DesktopFileProps.Name:
+                    desktopFile.name = desktopValue.trim()
+                    break
+                case DesktopFileProps.Icon:
+                    desktopFile.icon = desktopValue.trim()
+                    break
+                case DesktopFileProps.Exec:
+                    desktopFile.exec = desktopValue.trim()
+                    break
+                case DesktopFileProps.Type:
+                    desktopFile.type = desktopValue.trim()
+                    break
+            }
+        }
+    } catch (parsingError) {
+        console.error(parsingError)
+    }
+    return desktopFile
+}
+
+export async function generateInstalledAppsInfo(electronAppInfo: App) {
+    const installedApps = await getInstalledApps(electronAppInfo)
     const appInfo: AppInfo = {}
-    for (const appByPath of installedApps.split("\n")) {
-        if (appByPath) {
-            const [app, desktopPath] = appByPath.split(": ")
-            const appIcon = await findAppIcon(desktopPath)
-            const iconPath = appIcon ? appIcon.split("\n")[0] : ""
-            let b64Icon = ""
-            // todo: make concurrent via worker threads
-            if (iconPath) {
-                const imgData = await fs.readFile(iconPath)
-                const b64Data = Buffer.from(imgData).toString('base64')
-                const imgExt = iconPath.split(".").at(-1)
-                const mimeType = imgExt === "svg" ? "svg+xml" : imgExt
-                b64Icon = `data:image/${mimeType};base64,${b64Data}`
-            }
-            appInfo[app] = {
-                desktopPath,
-                iconPath,
-                b64Icon
-            }
+    // todo: make concurrent via worker threads
+    for (const desktopPath of installedApps) {
+        const desktopFile = await desktopFileParser(desktopPath)
+        const iconPath = await findAppIcon(desktopPath, desktopFile, electronAppInfo)
+        const b64Icon = await imgToB64(iconPath)
+        appInfo[desktopFile.name] = {
+            desktopPath,
+            iconPath,
+            b64Icon,
+            execCmd: desktopFile.exec
         }
     }
     return appInfo
